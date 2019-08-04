e806dc0cdf1a43820b514e72fed483b23e32fed5
zhaoyining <zyn0212@126.com>
2019-08-05 06:43:49 +0800

    add zyn_insertSort and zyn_bubbleSort function

--- zynfunc.c
+++ zynfunc.c
@@ -8,6 +8,8 @@
 #include "./zynfunc.h"
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
 extern int zyn_test(void)
 {
 	printf("test OK: %d!\n", zyn_gcd(134, 26));
@@ -65,4 +67,64 @@ extern long zyn_pow(int base, int x)
 		else if( 1 == (x & 1) )
 			ret *= tmp_base;
 	return ret;
+}
+int zyn_insertSort(void *unsort, int n, int size, int (*compare)(void const *a, void const *b), SORT_METHOD sortby)
+{
+	if( NULL == unsort || n < 2 || size < 1 || NULL == compare )
+		return -1;
+	void* sorted = NULL;
+	void* key = NULL;
+	if( NULL == (sorted = calloc(n, size)) || NULL == (key = calloc(1, size)) )
+		return -2;
+	int i = 0, j = 0, moved = 0;
+	memcpy(sorted, unsort, size);
+	for( i = 1; i < n; i += 1 )
+	{
+		memcpy(key, unsort + i * size, size);
+		j = i;
+		while( j > 0 && (compare( key, sorted + (j - 1) * size) * sortby) > 0 )
+		{
+			j -= 1;
+		}
+		moved += i != j;
+		memmove(sorted + (j + 1) * size, sorted + j * size, (i - j) * size);
+		memcpy(sorted + j * size, key, size);
+	}
+	memcpy(unsort, sorted, n * size);
+	free(sorted);
+	free(key);
+	return moved;
+}
+int zyn_bubbleSort(void *unsort, int n, int size, int (*compare)(void const *, void const *), SORT_METHOD sortby)
+{
+	if( n < 2 || size < 1 || NULL == unsort )
+		return -1;
+	void *temp = NULL;
+	void *sorted = NULL;
+	if( NULL == (temp = calloc(1, size)) || NULL == (sorted = calloc(n, size)) )
+		return -2;
+	memcpy(sorted, unsort, n * size);
+	int i = 0, j = 0, change = 0, moved = 0;
+	for( i = 1; i < n; i += 1, change = 0)
+	{
+		j = n - 1;
+		while( j >= i )
+		{
+			if( compare(sorted + j * size, sorted + (j - 1) * size) * sortby > 0 )
+			{
+				memcpy(temp, sorted + j * size, size);
+				memcpy(sorted + j * size, sorted + (j - 1) * size, size);
+				memcpy(sorted + (j - 1) * size, temp, size);
+				moved += 1;
+				change = 1;
+			}
+			j -= 1;
+		}
+		if( 0 == change )
+			break;
+	}
+	memcpy(unsort, sorted, n * size);
+	free(temp);
+	free(sorted);
+	return moved;
 }
--- zynfunc.h
+++ zynfunc.h
@@ -1,6 +1,10 @@
 #include <limits.h>
 #ifndef _LIBZYNFUNC_H
 #define _LIBZYNFUNC_H
+typedef enum {
+	SORT_LTOS = 1,
+	SORT_STOL = -1
+} SORT_METHOD;
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -11,6 +15,8 @@ extern int zyn_gcd(int a, int b);
 extern int zyn_lcm(int a, int b);
 extern int zyn_isPrime(int n);
 extern long zyn_pow(int base, int x);
+extern int zyn_insertSort(void *unsort, int n, int size, int (*compare)(void const *a, void const *b), SORT_METHOD sortby);
+extern int zyn_bubbleSort(void *unsort, int n, int size, int (*compare)(void const *a, void const *b), SORT_METHOD sortby);
 
 #ifdef __cplusplus
  }
